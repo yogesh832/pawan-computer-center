077ec62352b91640d261ef6115243be0b2ec4b7c
Yogesh832e <upadhayayyogesh832@gmail.com>
2024-07-28 14:30:00 +0530

    redirect changes

--- computer-center/src/App.jsx
+++ computer-center/src/App.jsx
@@ -1,10 +1,6 @@
 
 import './App.css'
-<<<<<<< HEAD
 import LandingContact from './components/LandingContact/LandingContact';
-=======
-import Contact from './components/contact/Contact';
->>>>>>> 80f364d96c35d6113b215154f6184ca367a6b04e
 import Nav from './components/navbar/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,11 +11,7 @@ function App() {
   return (
     <>
 <Nav/>
-<<<<<<< HEAD
 <LandingContact />
-=======
-<Contact/>
->>>>>>> 80f364d96c35d6113b215154f6184ca367a6b04e
     </>
   )
 }
