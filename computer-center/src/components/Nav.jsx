ad7085d8aa38a6b439e5a5d2614f787103386018
Yogesh832e <upadhayayyogesh832@gmail.com>
2024-07-28 10:41:25 +0530

    this is to add navbar to the page.

--- computer-center/src/components/Nav.jsx
+++ computer-center/src/components/Nav.jsx
@@ -1,56 +1,55 @@
 
 import style from "./Nav.module.css";
-
+import logo from "../assets/Images/logo.png";
 const Nav=()=>{ 
 
-
+const showList={
+  
+}
     return  <>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <img src="" alt="" />
-    <a className="navbar-brand" href="#">Navbar</a>
+<nav className={` ${style.navigation} navbar navbar-expand-lg bg-body-tertiary`}>
+  <div className={`${style.mainContainer} container-fluid `}>
+    <img className={style.logo} src={logo} alt="" />
+ 
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <span className="navbar-toggler-icon" 
+      // onClick={{showList}}
+      ></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        
+      <ul className="navbar-nav active">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link "  href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#">About Us</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#">Courses</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link " href="#">Our Service</a>
         </li>
         
-    
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+          <a className="nav-link " href="#">Certification</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
+          <a className="nav-link " href="#">Contact Us</a>
+        </li>  
        
+     
       </ul>
     </div>
   </div>
+  <div className={style.buttons}>
+  <button type="button" className={`${style.button1} btn btn-danger`}> Register Now</button>
+        <button type="button" className={`${style.button2} btn btn-danger`}> Student SignUp</button>
+        </div>
+        <div className={style.resButtons}>
+        <button type="button" className={`${style.resButton1} btn btn-danger`}>SignIn</button>
+        <button type="button" className={`${style.resButton2} btn btn-danger`}>SignUp</button>
+        </div>
 </nav>
 
       </>
