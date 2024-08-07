db4ae6e6729188a49c0e9ce2df8f7a8cd6a0a9c1
Yogesh832e <upadhayayyogesh832@gmail.com>
2024-08-07 17:08:28 +0530

    thsis is to edit

--- computer-center/src/components/Button.jsx
+++ computer-center/src/components/Button.jsx
@@ -7,6 +7,7 @@ const Buttongpt = ({ children, onClick, type = "button", className = "" }) => {
       onClick={onClick}
       className={`px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 ${className}`}
     >
+      {/* thsis is to edit */}
       {children}
     </button>
   );
