aad03dbed3b2b770329d2a47eb260350b34db9bc
Yogesh832e <upadhayayyogesh832@gmail.com>
2024-08-07 16:52:36 +0530

    added button to check tailwind

--- computer-center/src/components/Dashboard/Dashboard.jsx
+++ computer-center/src/components/Dashboard/Dashboard.jsx
@@ -4,14 +4,15 @@ import ControlPanel from './pannel/ControlPanel'
 import Sidebar from './Sidebar'
 import MarketingTools from './Marketing Tool/MarketingTool'
 import CenterMessage from './CenterMessage/CenterMessage'
-
+import Buttongpt from '../Button'
 
 export const Dashboard = () => {
   return (
     <>
     <div style={{display: 'flex', Height: '100vh'}}>
   {/* <Sidebar />   */}
-  <ControlPanel />
+  {/* <ControlPanel /> */}
+  <Buttongpt/>
  
   </div>
   {/* <MarketingTools />
