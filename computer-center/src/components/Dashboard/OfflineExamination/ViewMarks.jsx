06e03bad62fe743bc1a6146dba2b3629f68dd3a8
Virender Prasad <virender288@gmail.com>
2024-08-09 14:05:13 +0530

    added the scrolling feature

--- computer-center/src/components/Dashboard/OfflineExamination/ViewMarks.jsx
+++ computer-center/src/components/Dashboard/OfflineExamination/ViewMarks.jsx
@@ -10,12 +10,12 @@ const ViewMarks = () => {
         </h2>
       </div>
       <div className="p-4">
-        <div className="overflow-x-auto">
+        <div className="overflow-x-auto md:overflow-x-visible">
           <table className="min-w-full bg-white">
             <thead>
               <tr>
                 <th className="py-2 px-4 bg-blue-500 text-white font-bold">Sl.No.</th>
-                <th className="py-2 px-4 bg-blue-500 text-white font-bold">Student Uid	</th>
+                <th className="py-2 px-4 bg-blue-500 text-white font-bold">Student Uid</th>
                 <th className="py-2 px-4 bg-blue-500 text-white font-bold">Student Name</th>
                 <th className="py-2 px-4 bg-blue-500 text-white font-bold">Course</th>
                 <th className="py-2 px-4 bg-blue-500 text-white font-bold">Subject</th>
@@ -26,14 +26,15 @@ const ViewMarks = () => {
               </tr>
             </thead>
             <tbody>
-              
               <tr>
                 <td className="border px-4 py-2">1</td>
-                <td className="border px-4 py-2">RVSP2407105428	</td>
+                <td className="border px-4 py-2">RVSP2407105428</td>
                 <td className="border px-4 py-2">SANTOSH SINGH</td>
-                <td className="border px-4 py-2">Code : RV12023 Name : ADVANCE DIPLOMA IN COMPUTER APPLICATION -II Duration : 12 Months</td>
+                <td className="border px-4 py-2">Code: RV12023 Name: ADVANCE DIPLOMA IN COMPUTER APPLICATION -II Duration: 12 Months</td>
                 <td className="border px-4 py-2">HTML and CSS</td>
-                <td className="border px-4 py-2"><button className="bg-blue-600 p-2 rounded-md text-white">2</button></td>
+                <td className="border px-4 py-2">
+                    <button className="bg-blue-600 p-2 rounded-md text-white">2</button>
+                </td>
                 <td className="border px-4 py-2">56</td>
                 <td className="border px-4 py-2">25</td>
                 <td className="border px-4 py-2">10-08-2024</td>
