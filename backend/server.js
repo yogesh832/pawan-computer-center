356b876eba40e5f7b1b74ccf12d6081e5d7542f9
Yogesh832e <upadhayayyogesh832@gmail.com>
2024-10-08 08:32:17 +0530

    added cors access

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key"; // Use envir
 // Enable CORS for your frontend URL
 app.use(
   cors({
-    // origin: 'https://pawan-computer-center-h5xa.vercel.app/',
+    origin: 'https://pawan-computer-center-backend.vercel.app/login',
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // Allow cookies or other credentials
   })
